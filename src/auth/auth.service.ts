627956045b7504cdfb97abaaaba4710493809878
mmulyana <mulyan.t20@gmail.com>
2025-06-29 01:57:32 +0700

    feat: auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async registerUser(registerDto: RegisterDto): Promise<{ message: string }> {
+    const hashPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const userEmail = await this.userRepository.findOneBy({
+      email: registerDto.email,
+    });
+    const userName = await this.userRepository.findOneBy({
+      name: registerDto.name,
+    });
+
+    if (userEmail) {
+      throw new ConflictException('Email is already exists');
+    }
+    if (userName) {
+      throw new ConflictException('Name is already exists');
+    }
+
+    const newUser = this.userRepository.create({
+      ...registerDto,
+      password: hashPassword,
+    });
+
+    await this.userRepository.save(newUser);
+
+    return {
+      message: 'Register success',
+    };
+  }
+
+  async loginUser(loginDto: LoginDto): Promise<any> {
+    const user = await this.userRepository.findOne({
+      where: { email: loginDto.email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credential');
+    }
+
+    if (!(await bcrypt.compare(loginDto.password, user.password))) {
+      throw new UnauthorizedException('Invalid credential');
+    }
+
+    const payload = { id: user.id, email: user.email, role: user.role };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
