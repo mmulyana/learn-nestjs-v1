0088bd87261337beb3c8ada21749441be9b6b97a
mmulyana <mulyan.t20@gmail.com>
2025-06-29 10:18:54 +0700

    feat: simple rbac

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,9 @@ import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from './guard/auth.guard';
+import { RolesGuard } from './guard/role.guard';
+import { Roles } from './decolator/roles.decolator';
+import { Role } from './enum/role.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -29,5 +32,14 @@ export class AuthController {
   @Get('/getMe')
   async getUser(@Request() request): Promise<any> {
     return await this.authService.getUser(request.user.id);
+  }
+
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get('admin')
+  async getTestAdmin() {
+    return {
+      message: 'welcome admin',
+    };
   }
 }
